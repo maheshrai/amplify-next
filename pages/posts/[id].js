8f4d306fb994c602932e8eedd9fef6d4a19b9d8a
Mahesh Rai <mahesh.rai@aps.com>
2020-12-14 10:26:38 -0700

    NextJS AWS Amplify Workshop

---
+++ pages/posts/[id].js
@@ -0,0 +1,57 @@
+import { API } from "aws-amplify";
+import { useRouter } from "next/router";
+import ReactMarkdown from "react-markdown";
+import "../../configureAmplify";
+import { listPosts, getPost } from "../../graphql/queries";
+
+export default function Post({ post }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <div style={markdownStyle}>
+        <ReactMarkdown children={post.content} />
+      </div>
+      <p>Created by: {post.username}</p>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const postData = await API.graphql({
+    query: listPosts,
+  });
+  const paths = postData.data.listPosts.items.map((post) => ({
+    params: { id: post.id },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  const postData = await API.graphql({
+    query: getPost,
+    variables: { id },
+  });
+  return {
+    props: {
+      post: postData.data.getPost,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every second
+    revalidate: 1, // adds Incremental Static Generation, sets time in seconds
+  };
+}
+
+const markdownStyle = {
+  padding: 20,
+  border: "1px solid #ddd",
+  borderRadius: 5,
+};
